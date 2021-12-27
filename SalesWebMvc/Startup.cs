94208ac69ce57f44feb9c244e42c932cb0768f31
DiegoLins10 <diegofernandeslins@gmail.com>
2021-12-26 23:30:52 -0300

    criando um serviço para popular o bando de dados

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -31,14 +31,18 @@ namespace SalesWebMvc
             services.AddDbContext<SalesWebMvcContext>(options =>
             options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
             builder.MigrationsAssembly("SalesWebMvc")));
+
+            // Registrando servico no sistema de injecao de dependencia da aplicacao
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedingService seedService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedService.Seed();
             }
             else
             {
